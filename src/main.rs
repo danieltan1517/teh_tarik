596a39c402c5e242e144e82b2c8ec1900bac2441
Daniel Tan <danieltan@protonmail.com>
2023-04-26 18:00:22 -0700

    lexering.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 
 fn main() {
     // get commandline arguments.
@@ -29,25 +30,123 @@ fn main() {
 
     };
 
-    lexer(&high_level_code);
+    // lexing the code.
+    let mut code: &str = &high_level_code;
+    println!("High Level Code: ");
+    println!("{} ", code);
+    println!("----------------");
+    loop {
+      let (token, rest) = lexer(&code);
+      match token {
+      TokenType::FunctionKeyword => {
+        println!("function keyword");
+      }
+     
+      TokenType::Identifier(token) => {
+        println!("identifier [{}]", token);
+      }
+
+      TokenType::LeftParenthesis => {
+        println!("left parenthesis '('");
+      }
+
+      TokenType::RightParenthesis => {
+        println!("right parenthesis ')'");
+      }
+
+      TokenType::LeftCurlyBrace => {
+        println!("left curly brace '{{'");
+      }
+
+      TokenType::RightCurlyBrace => {
+        println!("right curly brace '}}'");
+      }
+     
+      TokenType::Error(message) => {
+        println!("**Error: {}", message);
+      }
+     
+      TokenType::EOF => {
+        println!("End of file");
+        break;
+      }
+     
+      }
+      code = rest;
+    }
 }
 
-enum TokenType<'a> {
-  FunctionKeyword(&'a str),
+enum TokenType {
+  FunctionKeyword,
+  LeftParenthesis,
+  RightParenthesis,
+  LeftCurlyBrace,
+  RightCurlyBrace,
+  Identifier(String),
+  Error(String),
   EOF
 }
 
-
 fn lexer(code: &str) -> (TokenType, &str) {
-    println!("Here is the high level code: ");
-    println!("{}", code);
+    let mut state = StateMachine::Init;
+    for (i, chr) in code.chars().enumerate() {
+        match state {
+        StateMachine::Init => {
+          if chr.is_whitespace() {
+            state = StateMachine::Init;
+          } else if chr >= '0' && chr <= '9' {
+            state = StateMachine::Number;
+          } else if chr.is_alphabetic() {
+            state = StateMachine::Identifier;
+          } else if chr == '(' {
+            let rest  = &code[i + 1..];
+            return (TokenType::LeftParenthesis, rest);
+          } else if chr == ')' {
+            let rest  = &code[i + 1..];
+            return (TokenType::RightParenthesis, rest);
+          } else if chr == '{' {
+            let rest  = &code[i + 1..];
+            return (TokenType::LeftCurlyBrace, rest);
+          } else if chr == '}' {
+            let rest  = &code[i + 1..];
+            return (TokenType::RightCurlyBrace, rest);
+          }
+        }
+       
+        StateMachine::Number => {
+          if chr >= '0' && chr <= '9' {
+            state = StateMachine::Number;
+          } else {
+            let error_string = String::from("Numbers cannot have letters inside of them.");
+            return (TokenType::Error(error_string), "");
+          }
+        }
+       
+        StateMachine::Identifier => {
+          if chr.is_whitespace() {
+            let token = String::from(&code[0..i]);
+            let rest  = &code[i..];
+            return (TokenType::Identifier(token), rest);
+          } else if chr.is_alphabetic() || chr == '_' ||  (chr >= '0' && chr <= '9') {
+            state = StateMachine::Identifier;
+          } else {
+            let token = String::from(&code[0..i]);
+            let rest  = &code[i..];
+            return (TokenType::Identifier(token), rest);
+          }
+        }
 
-    for chr in code.chars() {
-      println!("{}", chr);
+        }
     }
 
     // split_at()
-    return (TokenType::EOF, code);
+    return (TokenType::EOF, "");
+
+    enum StateMachine {
+      Init,
+      Number,
+      Identifier,
+    }
 }
 
 
