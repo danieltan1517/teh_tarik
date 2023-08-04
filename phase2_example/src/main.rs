bd7f2f3c01a38a131b3163cd73baf969028203c4
Daniel Tan <danieltan@protonmail.com>
2023-08-04 10:03:46 -0700

    unit tests.

--- phase2_example/src/main.rs
+++ phase2_example/src/main.rs
@@ -538,4 +538,28 @@ fn parse_term(tokens: &Vec<Token>, index: &mut usize) -> Result<(), String> {
 }
 
 
+// writing tests!
+#[cfg(test)]
+mod tests {
+    use crate::lex;
+    use crate::parse_statement;
+
+    #[test]
+    fn test_statements() {
+
+        // test that valid statements are correct.
+        let (tokens,_) = lex("a = 1 + 2;").unwrap();
+        parse_statement(&tokens, &mut 0).unwrap();
+
+        let (tokens,_) = lex("b = 1 / 2;").unwrap();
+        parse_statement(&tokens, &mut 0).unwrap();
+
+
+        // test errors. missing semicolon
+        let (tokens,_) = lex("b = 1 / 2").unwrap();
+        assert!(matches!(parse_statement(&tokens, &mut 0), Err(_)));
+
+    }
+
+}
 
