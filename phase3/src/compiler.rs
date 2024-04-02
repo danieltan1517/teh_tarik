60534d2618c7883a6bcf9d9c778b5dc3e2cfbd4c
Daniel Tan <danieltan1517@protonmail.com>
2024-04-01 21:18:57 -0700

    array zero.

--- phase3/src/compiler.rs
+++ phase3/src/compiler.rs
@@ -654,8 +654,15 @@ fn parse_instruction(serialized_line: &mut usize, line: usize, function: &mut Fu
 
         let num = match next_result(*serialized_line, tokens, idx)? {
         IRTok::Num(num) => *num,
+        IRTok::Var(ident) => {
+          return error(*serialized_line, String::from("array cannot be a variable '{ident}' length. must be a number.")),
+        }
         _ => return error(*serialized_line, String::from("invalid instruction. expected format like '%int[] array, 10'")),
         };
+
+        if num <= 0 {
+          return error(*serialized_line, String::from("array size cannot be less than or equal to zero.")),
+        }
 
         if let Some(_) = function.variables.get(ident) {
              let f = format!("invalid instruction. identifier '{}' declared too many times", ident);
