2f3309012f646b1fe73189486991a6a8ad9aa796
Daniel Tan <danieltan@protonmail.com>
2024-04-01 20:24:42 -0700

    changes to phase 4.

--- phase3/src/main.rs
+++ phase3/src/main.rs
@@ -30,7 +30,7 @@ fn main() {
 
     };
 
-    let (tokens, location) = match lex(&code) {
+    let tokens = match lex(&code) {
     Err(error_message) => {
         println!("**Error**");
         println!("----------------------");
@@ -39,7 +39,7 @@ fn main() {
         return;
     }
 
-    Ok(data) => data,
+    Ok(tokens) => tokens,
     
     };
     
@@ -59,15 +59,8 @@ fn main() {
         println!("----------------------");
         if tokens.len() == 0 {
             println!("No code has been provided.");
-        } else if index >= tokens.len() {
-            index = tokens.len() - 1;
-            let loc = &location[index];
-            println!("Error at line {}:{}. {}", loc.line, loc.col, message);
-            println!("----------------------");
         } else {
-            index -= 1;
-            let loc = &location[index];
-            println!("Error at line {}:{}. {}", loc.line, loc.col, message);
+            println!("Error: {}", message);
             println!("----------------------");
         }
     }
@@ -77,12 +70,15 @@ fn main() {
 
 #[derive(Debug)]
 enum Token {
+  NotToken,
   // keywords:
   Func,
   Return,
   Int,
   Print,
+  While,
   Read,
+  If,
 
   LeftParen,
   RightParen,
@@ -103,6 +99,301 @@ enum Token {
   Num(i32),
 }
 
+// This is a lexer that parses numbers/identifiers and math operations
+fn lex(mut code: &str) -> Result<Vec<Token>, String> {
+  let mut tokens: Vec<Token> = vec![];
+  while code.len() > 0 {
+    let (success, token, rest) = lex_number(code);
+    if success {
+      code = rest; 
+      tokens.push(token);
+      continue;
+    } 
+ 
+    let (success, rest) = lex_space(code);
+    if success {
+      code = rest;
+      continue;
+    }
+
+    if code.starts_with("+") {
+      code = &code[1..];
+      tokens.push(Token::Plus);
+      continue;
+    }
+
+    if code.starts_with("-") {
+      code = &code[1..];
+      tokens.push(Token::Subtract);
+      continue;
+    }
+
+    if code.starts_with(";") {
+      code = &code[1..];
+      tokens.push(Token::Semicolon);
+      continue;
+    }
+
+    if code.starts_with("*") {
+      code = &code[1..];
+      tokens.push(Token::Multiply);
+      continue;
+    }
+
+    if code.starts_with("/") {
+      code = &code[1..];
+      tokens.push(Token::Divide);
+      continue;
+    }
+
+    if code.starts_with("(") {
+      code = &code[1..];
+      tokens.push(Token::LeftParen);
+      continue;
+    }
+
+    if code.starts_with("{") {
+      code = &code[1..];
+      tokens.push(Token::LeftCurly);
+      continue;
+    }
+
+    if code.starts_with("}") {
+      code = &code[1..];
+      tokens.push(Token::RightCurly);
+      continue;
+    }
+   
+    if code.starts_with(")") {
+      code = &code[1..];
+      tokens.push(Token::RightParen);
+      continue;
+    }
+
+    if code.starts_with("%") {
+      code = &code[1..];
+      tokens.push(Token::Modulus);
+      continue;
+    }
+
+    if code.starts_with(",") {
+      code = &code[1..];
+      tokens.push(Token::Comma);
+      continue;
+    }
+
+    if code.starts_with("=") {
+      code = &code[1..];
+      tokens.push(Token::Assign);
+      continue;
+    }
+
+    let (success, rest) = lex_comment(code);
+    if success {
+      code = rest;
+      continue;
+    }
+
+    let (success, token, rest) = lex_identifier(code);
+    if success {
+      code = rest;
+      tokens.push(token);
+      continue;
+    }
+
+    let symbol = unrecognized_symbol(code);
+    return Err(format!("Unidentified symbol {symbol}"));
+
+  }
+
+  return Ok(tokens);
+}
+
+fn lex_space(code: &str) -> (bool, &str) {
+  for letter in code.chars() {
+    if letter.is_whitespace() {
+      return (true, &code[1..]);
+    } else {
+      return (false, code);
+    }
+  }
+  return (false, code);
+}
+
+// lex numbers.
+fn lex_number(code: &str) -> (bool, Token, &str) {
+  enum StateMachine {
+    Start,
+    Number,
+  }
+
+  let mut success = false;
+  let mut state = StateMachine::Start;
+  let mut index = 0;
+  for letter in code.chars() {
+    match state {
+    StateMachine::Start => {
+      if letter >= '0' && letter <= '9' {
+        state = StateMachine::Number;
+        success = true;
+        index += 1;
+      } else {
+        return (false, Token::NotToken, "");
+      }
+    }
+
+    StateMachine::Number => {
+      if letter >= '0' && letter <= '9' {
+        state = StateMachine::Number;
+        success = true;
+        index += 1;
+      } else {
+        let num = code[..index].parse::<i32>().unwrap();
+        return (true, Token::Num(num), &code[index..]);
+      }
+    }
+
+    }
+  }
+
+  if success == true {
+    let num: i32 = code.parse::<i32>().unwrap();
+    return (true, Token::Num(num), "");
+  } else {
+    return (false, Token::NotToken, "");
+  }
+}
+
+// lex identifiers.
+fn lex_identifier(code: &str) -> (bool, Token, &str) {
+  enum StateMachine {
+    Start,
+    Ident,
+  }
+
+  let mut success = false;
+  let mut state = StateMachine::Start;
+  let mut index = 0;
+  for letter in code.chars() {
+    match state {
+    StateMachine::Start => {
+      if (letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'){
+        state = StateMachine::Ident;
+        success = true;
+        index += 1;
+      } else {
+        return (false, Token::NotToken, "");
+      }
+    }
+
+    StateMachine::Ident => {
+      if (letter >= 'A' && letter <= 'Z') || (letter >= 'a' && letter <= 'z') || (letter >= '0' && letter <= '9') || letter == '_' {
+        state = StateMachine::Ident;
+        success = true;
+        index += 1;
+      } else {
+        let token = &code[..index];
+        return (true, create_identifier(token), &code[index..]);
+      }
+    }
+
+    }
+  }
+
+  if success == true {
+    return (true, create_identifier(code), "");
+  } else {
+    return (false, Token::NotToken, "");
+  }
+}
+
+// lex identifiers.
+fn lex_comment(code: &str) -> (bool, &str) {
+  #[derive(Debug)]
+  enum StateMachine {
+    Start,
+    Comment,
+  }
+
+  let mut success = false;
+  let mut state = StateMachine::Start;
+  let mut index = 0;
+  for letter in code.chars() {
+    match state {
+    StateMachine::Start => {
+      if letter == '#' {
+        state = StateMachine::Comment;
+        success = true;
+        index += 1;
+      } else {
+        return (false, "");
+      }
+    }
+
+    StateMachine::Comment => {
+      if letter != '\n' {
+        state = StateMachine::Comment;
+        success = true;
+        index += 1;
+      } else {
+        return (true, &code[index..]);
+      }
+    }
+
+    }
+  }
+
+  if success == true {
+    return (true, &code[index..]);
+  } else {
+    return (false, "");
+  }
+}
+
+
+
+fn create_identifier(code: &str) -> Token {
+  match code {
+  "func" => Token::Func,
+  "return" => Token::Return,
+  "int" => Token::Int,
+  "print" => Token::Print,
+  "read" => Token::Read,
+  "while" => Token::While,
+  "if" => Token::If,
+  _ => Token::Ident(String::from(code)),
+  }
+}
+
+fn unrecognized_symbol(code: &str) -> &str {
+  enum StateMachine {
+    Start,
+    Symbol,
+  }
+
+  let mut state_machine = StateMachine::Start;
+  let mut index = 0;
+  for letter in code.chars() {
+    match state_machine {
+    StateMachine::Start => {
+      state_machine = StateMachine::Symbol;
+      index += 1;
+    } 
+    
+    StateMachine::Symbol => {
+      if letter.is_whitespace() {
+        return &code[..index];
+      } else {
+        index += 1;
+      }
+    }
+
+    }
+  }
+  return &code[..index];
+} 
+
+
 struct Expression {
   code: String,
   name: String,
@@ -115,175 +406,6 @@ fn create_temp() -> String {
         VAR_NUM += 1;
         format!("_temp{}", VAR_NUM)
     }
-}
-
-struct Loc {
-  line: i32,
-  col:  i32,
-}
-
-fn lex(code: &str) -> Result<(Vec<Token>, Vec<Loc>), String> {
-    let mut tokens: Vec<Token> = vec![];
-    let mut locations: Vec<Loc> = vec![];
-    let mut token_start: usize = 0;
-    let mut token_end:   usize = 0;
-    let mut line_num:    i32   = 1;
-    let mut col_num:     i32   = 1;
-    let mut state_machine = StateMachine::Init;
-
-    for character in code.chars() {
-
-        // transitions.
-        state_machine = match state_machine {
-
-        StateMachine::Init => {
-            token_start = token_end;
-            if character.is_alphabetic() {
-                StateMachine::Ident
-            } else if character >= '0' && character <= '9' {
-                StateMachine::Number
-            } else if character == '#' {
-                StateMachine::Comment
-            } else {
-                StateMachine::Init
-            }
-        }
-
-        StateMachine::Number => {
-            if character >= '0' && character <= '9' {
-                StateMachine::Number
-            } else if character.is_alphabetic() || character == '_' {
-                StateMachine::ErrorNum
-            } else {
-                let number = create_number(token_start, token_end, code);
-                add(&mut tokens, &mut locations, Token::Num(number), line_num, col_num);
-                StateMachine::Init
-            }
-        }
-
-        StateMachine::Ident => {
-            if character.is_alphabetic() || (character >= '0' && character <= '9') || character == '_' {
-                StateMachine::Ident
-            } else {
-                let ident = create_identifier(token_start, token_end, code);
-                add(&mut tokens, &mut locations, ident, line_num, col_num);
-                token_start = token_end;
-                StateMachine::Init
-            }
-        }
-
-        StateMachine::ErrorNum => StateMachine::ErrorNum,
-
-        StateMachine::Comment => {
-            if character == '\n' {
-                StateMachine::Init
-            } else {
-                StateMachine::Comment
-            }
-        }
-
-        };
-
-        token_end += 1;
-
-        // actions of state machine.
-        match state_machine {
-
-        StateMachine::Init => {
-             match character {
-             '+' => add(&mut tokens, &mut locations, Token::Plus, line_num, col_num),
-             '-' => add(&mut tokens, &mut locations, Token::Subtract, line_num, col_num),
-             '*' => add(&mut tokens, &mut locations, Token::Multiply, line_num,col_num),
-             '/' => add(&mut tokens, &mut locations, Token::Divide, line_num, col_num),
-             '%' => add(&mut tokens, &mut locations, Token::Modulus, line_num, col_num),
-             ',' => add(&mut tokens, &mut locations, Token::Comma, line_num, col_num),
-             '{' => add(&mut tokens, &mut locations, Token::LeftCurly, line_num, col_num),
-             '}' => add(&mut tokens, &mut locations, Token::RightCurly, line_num, col_num),
-             '(' => add(&mut tokens, &mut locations, Token::LeftParen, line_num, col_num),
-             ')' => add(&mut tokens, &mut locations, Token::RightParen, line_num, col_num),
-             ';' => add(&mut tokens, &mut locations, Token::Semicolon, line_num, col_num),
-             '=' => add(&mut tokens, &mut locations, Token::Assign, line_num, col_num),
-              _  => {
-                 if !character.is_whitespace() {
-                     let ident = &code[token_start..token_end];
-                     return Err(format!("Error at line {}:{}. Unidentified symbol '{}'", line_num, col_num, ident));
-                 }
-             }
-
-             }
-        }
-
-        StateMachine::Number => {}
-        StateMachine::Comment => {}
-        StateMachine::Ident => {}
-
-        StateMachine::ErrorNum => {
-            if character == ' ' {
-                let ident = &code[token_start..token_end];
-                return Err(format!("Error at line {}:{}. Invalid Number '{}'", line_num, col_num, ident));
-            }
-        }
-
-        };
-
-        if character == '\n' {
-            col_num = 1;
-            line_num += 1;
-        } else {
-            col_num += 1;
-        }
-    }
-
-    match state_machine {
-    StateMachine::Number => {
-        let number = create_number(token_start, token_end, code);
-        add(&mut tokens, &mut locations, Token::Num(number), line_num, col_num);
-    }
-    StateMachine::Ident => {
-        let ident = create_identifier(token_start, token_end, code);
-        add(&mut tokens, &mut locations, ident, line_num, col_num);
-    }
-    _ => {}
-    }
-
-    return Ok((tokens, locations));
-
-    // helper functions
-    fn create_identifier(token_start: usize, token_end: usize, code: &str) -> Token {
-        let token = &code[token_start..token_end];
-        match token {
-        "func" => Token::Func,
-        "return" => Token::Return,
-        "int" => Token::Int,
-        "print" => Token::Print,
-        "read" => Token::Read,
-        _ => Token::Ident(String::from(token)),
-        }
-    }
-
-    fn add(tokens: &mut Vec<Token>, locations: &mut Vec<Loc>, tok: Token, line: i32, col: i32) {
-        tokens.push(tok);
-        locations.push(Loc{line, col});
-    }
-
-    fn create_number(token_start: usize, token_end: usize, code: &str) -> i32 {
-        // this code should correctly parse because the lexer verified that this is correct.
-        // quit.
-        let token = &code[token_start..token_end];
-        match token.parse::<i32>() {
-        Err(_) => panic!("Error. Logic Error: Lexer failed to lex number \"{token}\" correctly"),
-        Ok(num) => num,
-        }
-    }
-
-    enum StateMachine {
-        Init,
-        Number,
-        Ident,
-        Comment,
-        ErrorNum,
-    }
-
 }
 
 // the <'a> is the "lifetimes" type annotations in Rust.
