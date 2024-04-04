affc8d1f8beed3a4bee1515798c9cc2cfc19bcf4
Daniel Tan <danieltan1517@protonmail.com>
2024-04-04 13:01:34 -0700

    fixes.

--- phase3/src/compiler.rs
+++ phase3/src/compiler.rs
@@ -655,7 +655,7 @@ fn parse_instruction(serialized_line: &mut usize, line: usize, function: &mut Fu
         let num = match next_result(*serialized_line, tokens, idx)? {
         IRTok::Num(num) => *num,
         IRTok::Var(ident) => {
-          return error(*serialized_line, String::from("array cannot be a variable '{ident}' length. must be a number."));
+          return error(*serialized_line, format!("array cannot be a variable '{ident}' length. must be a number."));
         }
         _ => {return error(*serialized_line, String::from("invalid instruction. expected format like '%int[] array, 10'"));}
         };
