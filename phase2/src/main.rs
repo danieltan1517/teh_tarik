03cf4d8fb89b0d4322944dd8db3f692898bae738
Daniel Tan <danieltan1517@protonmail.com>
2024-10-02 21:08:21 +0000

    made phase2 less confusing.

--- phase2/src/main.rs
+++ phase2/src/main.rs
@@ -2,10 +2,10 @@ use std::env;
 use std::fs;
 
 fn main() {
-    // Let us get commandline arguments and store them in a Vec<String>
+    // get commandline arguments.
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
-        println!("Please provide an input file through the commandline arguments for the lexer.");
+        println!("Please provide an input file.");
         return;
     }
 
@@ -14,16 +14,17 @@ fn main() {
         return;
     }
 
-    // read the entire file contents, storing them inside 'code' as a string.
+    // read the entire file.
     let filename = &args[1];
-    let code = match fs::read_to_string(filename) {
+    let result = fs::read_to_string(filename);
+    let code = match result {
     Err(error) => {
         println!("**Error. File \"{}\": {}", filename, error);
         return;
     }
 
     Ok(code) => {
-        code
+      code
     } 
 
     };
@@ -40,48 +41,57 @@ fn main() {
     Ok(tokens) => tokens,
     
     };
-
+    
     let mut index: usize = 0;
-    match parse_math(&tokens, &mut index) {
-    Ok(num) => {
-        println!("Expression = {code}");
-        println!("Answer = {num}");
+    match parse_program(&tokens, &mut index) {
+
+    Ok(()) => {
+        println!("Program Parsed Successfully.");
     }
 
-    Err(e) => {
+    Err(message) => {
+        println!("**Error**");
+        println!("----------------------");
         if tokens.len() == 0 {
             println!("No code has been provided.");
         } else {
-            println!("Error {e}");
+            println!("Error: {message}");
+            println!("----------------------");
         }
     }
 
     }
-
-
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 enum Token {
   NotToken,
+  // keywords:
+  Func,
+  Return,
+  Int,
+  Print,
+  While,
+  Read,
+  If,
+
+  LeftParen,
+  RightParen,
+  LeftCurly,
+  RightCurly,
+  Comma,
+  Semicolon,
+
+  // mathematical operators.
   Plus,
   Subtract,
   Multiply,
   Divide,
   Modulus,
-  LeftParen,
-  RightParen,
-  Num(i32),
-  Ident(String),
-  If,
-  While,
-  Read,
-  Write,
-  Return,
-  Func,
   Assign,
-  Int,
-  Semicolon,
+
+  Ident(String),
+  Num(i32),
 }
 
 // This is a lexer that parses numbers/identifiers and math operations
@@ -113,6 +123,12 @@ fn lex(mut code: &str) -> Result<Vec<Token>, String> {
       continue;
     }
 
+    if code.starts_with(";") {
+      code = &code[1..];
+      tokens.push(Token::Semicolon);
+      continue;
+    }
+
     if code.starts_with("*") {
       code = &code[1..];
       tokens.push(Token::Multiply);
@@ -125,9 +141,39 @@ fn lex(mut code: &str) -> Result<Vec<Token>, String> {
       continue;
     }
 
+    if code.starts_with("(") {
+      code = &code[1..];
+      tokens.push(Token::LeftParen);
+      continue;
+    }
+
+    if code.starts_with("{") {
+      code = &code[1..];
+      tokens.push(Token::LeftCurly);
+      continue;
+    }
+
+    if code.starts_with("}") {
+      code = &code[1..];
+      tokens.push(Token::RightCurly);
+      continue;
+    }
+   
+    if code.starts_with(")") {
+      code = &code[1..];
+      tokens.push(Token::RightParen);
+      continue;
+    }
+
     if code.starts_with("%") {
       code = &code[1..];
       tokens.push(Token::Modulus);
+      continue;
+    }
+
+    if code.starts_with(",") {
+      code = &code[1..];
+      tokens.push(Token::Comma);
       continue;
     }
 
@@ -137,21 +183,9 @@ fn lex(mut code: &str) -> Result<Vec<Token>, String> {
       continue;
     }
 
-    if code.starts_with("(") {
-      code = &code[1..];
-      tokens.push(Token::LeftParen);
-      continue;
-    }
-
-    if code.starts_with(")") {
-      code = &code[1..];
-      tokens.push(Token::RightParen);
-      continue;
-    }
-
-    if code.starts_with(";") {
-      code = &code[1..];
-      tokens.push(Token::Semicolon);
+    let (success, rest) = lex_comment(code);
+    if success {
+      code = rest;
       continue;
     }
 
@@ -268,6 +302,64 @@ fn lex_identifier(code: &str) -> (bool, Token, &str) {
   }
 }
 
+// lex identifiers.
+fn lex_comment(code: &str) -> (bool, &str) {
+  #[derive(Debug)]
+  enum StateMachine {
+    Start,
+    Comment,
+  }
+
+  let mut success = false;
+  let mut state = StateMachine::Start;
+  let mut index = 0;
+  for letter in code.chars() {
+    match state {
+    StateMachine::Start => {
+      if letter == '#' {
+        state = StateMachine::Comment;
+        success = true;
+        index += 1;
+      } else {
+        return (false, "");
+      }
+    }
+
+    StateMachine::Comment => {
+      if letter != '\n' {
+        state = StateMachine::Comment;
+        success = true;
+        index += 1;
+      } else {
+        return (true, &code[index..]);
+      }
+    }
+
+    }
+  }
+
+  if success == true {
+    return (true, &code[index..]);
+  } else {
+    return (false, "");
+  }
+}
+
+
+
+fn create_identifier(code: &str) -> Token {
+  match code {
+  "func" => Token::Func,
+  "return" => Token::Return,
+  "int" => Token::Int,
+  "print" => Token::Print,
+  "read" => Token::Read,
+  "while" => Token::While,
+  "if" => Token::If,
+  _ => Token::Ident(String::from(code)),
+  }
+}
+
 fn unrecognized_symbol(code: &str) -> &str {
   enum StateMachine {
     Start,
@@ -296,23 +388,8 @@ fn unrecognized_symbol(code: &str) -> &str {
   return &code[..index];
 } 
 
-fn create_identifier(code: &str) -> Token {
-  match code {
-  "func" => Token::Func,
-  "return" => Token::Return,
-  "int" => Token::Int,
-  "print" => Token::Write,
-  "read" => Token::Read,
-  "while" => Token::While,
-  "if" => Token::If,
-  _ => Token::Ident(String::from(code)),
-  }
-}
-
 // the <'a> is the "lifetimes" type annotations in Rust.
 //
-// this 'dead_code' macro is just to supress Rust's dead_code warning. This macro can be removed.
-#[allow(dead_code)]
 fn peek<'a>(tokens: &'a Vec<Token>, index: usize) -> Option<&'a Token> {
     if index < tokens.len() {
         return Some(&tokens[index])
@@ -329,8 +406,6 @@ fn peek_result<'a>(tokens: &'a Vec<Token>, index: usize) -> Result<&'a Token, St
     }
 }
 
-// this 'dead_code' macro is just to supress Rust's dead_code warning. This macro can be removed.
-#[allow(dead_code)]
 fn next<'a>(tokens: &'a Vec<Token>, index: &mut usize) -> Option<&'a Token> {
     if *index < tokens.len() {
         let ret = *index;
@@ -351,28 +426,212 @@ fn next_result<'a>(tokens: &'a Vec<Token>, index: &mut usize) -> Result<&'a Toke
     }
 }
 
-fn parse_math(tokens: &Vec<Token>, index: &mut usize) -> Result<i32, String> {
-  let answer = parse_expression(tokens, index)?;
-  if matches!(next_result(tokens, index)?, Token::Semicolon) {
-    return Ok(answer);
-  } else {
-    return Err(String::from("missing semicolon ';'"));
-  }
+// parse programs with multiple functions
+// loop over everything, outputting generated code.
+fn parse_program(tokens: &Vec<Token>, index: &mut usize) -> Result<(), String> {
+    loop {
+        match parse_function(tokens, index)? {
+        None => {
+            break;
+        }
+        Some(_) => {}
+        }
+    }
+
+    return Ok(());
 }
 
-fn parse_expression(tokens: &Vec<Token>, index: &mut usize) -> Result<i32, String> {
-    let mut answer = parse_multiply_expression(tokens, index)?;
+// parse function such as:
+// func main(int a, int b) {
+//    # ... statements here...
+//    # ...
+// }
+// a loop is done to handle statements.
+
+fn parse_function(tokens: &Vec<Token>, index: &mut usize) -> Result<Option<()>, String> {
+    
+    match next(tokens, index) {
+    None => {
+        return Ok(None);
+    }
+    Some(token) => {
+        if !matches!(token, Token::Func) {
+            return Err(String::from("functions must begin with func"));
+        }
+    }
+
+    }
+    match next_result(tokens, index)? {
+    Token::Ident(_) => {},
+    _  => {return Err(String::from("functions must have a function identifier"));}
+    };
+
+    if !matches!( next_result(tokens, index)?, Token::LeftParen) {
+        return Err(String::from("expected '('"));
+    }
+
+    loop {
+       match next_result(tokens, index)? {
+
+       Token::RightParen => {
+           break;
+       }
+
+       Token::Int => {
+           match next_result(tokens, index)? {
+           Token::Ident(_) => {
+               match peek_result(tokens, *index)? {
+               Token::Comma => {
+                   *index += 1;
+               }
+               Token::RightParen => {}
+               _ => {
+                   return Err(String::from("expected ',' or ')'"));
+               }
+
+               }
+           }
+           _ => {
+                return Err(String::from("expected ident function parameter"));
+           }
+
+           }
+       }
+
+       _ => {
+           return Err(String::from("expected 'int' keyword or ')' token"));
+       }
+
+       }
+    }
+
+
+    if !matches!(next_result(tokens, index)?, Token::LeftCurly) {
+        return Err(String::from("expected '{'"));
+    }
+
+    loop {
+        match parse_statement(tokens, index)? {
+        None => {
+            break;
+        }
+        Some(()) => {}
+        }
+    }
+
+
+    if !matches!(next_result(tokens, index)?, Token::RightCurly) {
+      return Err(String::from("expected '}'"));
+    }
+
+    return Ok(Some(()));
+}
+
+// parsing a statement such as:
+// int a;
+// a = a + b;
+// a = a % b;
+// print(a)
+// read(a)
+// returns epsilon if '}'
+fn parse_statement(tokens: &Vec<Token>, index: &mut usize) -> Result<Option<()>, String> {
+    match peek(tokens, *index) {
+    None => {
+        return Ok(None);
+    }
+
+    Some(token) => {
+        match token {
+
+        Token::RightCurly => {
+            return Ok(None);
+        }
+
+        Token::Int => {
+            *index += 1;
+            match next_result(tokens, index)? {
+            Token::Ident(_) => {}
+
+            _ => {
+                return Err(String::from("expected identifier"));
+            }
+
+            }
+        }
+
+        Token::Ident(_) => {
+            *index += 1;
+            if !matches!(next_result(tokens, index)?, Token::Assign) {
+                return Err(String::from("expected '=' assignment operator"));
+            }
+            parse_expression(tokens, index)?;
+        }
+
+        Token::Return => {
+            *index += 1;
+            parse_expression(tokens, index)?;
+        }
+
+        Token::Print => {
+            *index += 1;
+            if !matches!(next_result(tokens, index)?, Token::LeftParen) {
+                return Err(String::from("expect '(' closing statement"));
+            }
+            parse_expression(tokens, index)?;
+            if !matches!(next_result(tokens, index)?, Token::RightParen) {
+                return Err(String::from("expect ')' closing statement"));
+            }
+        }
+
+        Token::Read => {
+            *index += 1;
+            if !matches!(next_result(tokens, index)?, Token::LeftParen) {
+                return Err(String::from("expect '(' closing statement"));
+            }
+
+            parse_expression(tokens, index)?;
+
+            if !matches!(next_result(tokens, index)?, Token::RightParen) {
+                return Err(String::from("expect ')' closing statement"));
+            }
+        }
+
+        _ => {
+             return Err(String::from("invalid statement."));
+        }
+
+        }
+        if !matches!(next_result(tokens, index)?, Token::Semicolon) {
+            return Err(String::from("expect ';' closing statement"));
+        }
+
+        return Ok(Some(()));
+    }
+
+    }
+}
+
+// parsing a simple expression such as:
+// "a" (alone)
+// "a + b"
+// "a * b"
+// "a - b"
+// NOTE: this cannot parse "complex" expressions such as "a + b * c".
+// I leave "a + b * c" as an exercise for the student.
+fn parse_expression(tokens: &Vec<Token>, index: &mut usize) -> Result<(), String> {
+
+    parse_multiply_expression(tokens, index)?;
     loop {
        match peek_result(tokens, *index)? {
 
        Token::Plus => {
            *index += 1;
-           answer += parse_multiply_expression(tokens, index)?;
+           parse_multiply_expression(tokens, index)?;
        }
 
        Token::Subtract => {
            *index += 1;
-           answer -= parse_multiply_expression(tokens, index)?;
+           parse_multiply_expression(tokens, index)?;
        }
 
        _ => { 
@@ -382,26 +641,26 @@ fn parse_expression(tokens: &Vec<Token>, index: &mut usize) -> Result<i32, Strin
        };
     }
 
-    return Ok(answer);
+    return Ok(());
 }
 
-fn parse_multiply_expression(tokens: &Vec<Token>, index: &mut usize) -> Result<i32, String> {
-    let mut answer = parse_term(tokens, index)?;
+fn parse_multiply_expression(tokens: &Vec<Token>, index: &mut usize) -> Result<(), String> {
+    parse_term(tokens, index)?;
     loop {
        match peek_result(tokens, *index)? {
        Token::Multiply => {
           *index += 1;
-          answer *= parse_term(tokens, index)?;
+          parse_term(tokens, index)?;
        }
 
        Token::Divide => {
           *index += 1;
-          answer /= parse_term(tokens, index)?;
+          parse_term(tokens, index)?;
        }
 
        Token::Modulus => {
           *index += 1;
-          answer %= parse_term(tokens, index)?;
+          parse_term(tokens, index)?;
        }
   
        _ => {
@@ -412,72 +671,60 @@ fn parse_multiply_expression(tokens: &Vec<Token>, index: &mut usize) -> Result<i
 
     }
 
-    return Ok(answer);
+    return Ok(());
 }
 
-fn parse_term(tokens: &Vec<Token>, index: &mut usize) -> Result<i32, String> {
+// a term is either a Number or an Identifier.
+fn parse_term(tokens: &Vec<Token>, index: &mut usize) -> Result<(), String> {
     match next_result(tokens, index)? {
 
-    Token::Num(num) => {
-        return Ok(*num);
+    Token::Ident(_) => {
+        return Ok(());
+    }
+
+    Token::Num(_) => {
+        return Ok(());
     }
 
     Token::LeftParen => {
-        println!("Expression term\n");
-        let answer = parse_expression(tokens, index)?;
+        parse_expression(tokens, index)?;
         if !matches!(next_result(tokens, index)?, Token::RightParen) {
-            return Err(String::from("expected ')' parenthesis"));
+            return Err(String::from("missing right parenthesis ')'"));
         }
-        return Ok(answer);
+
+        return Ok(());
     }
 
     _ => {
-        println!("{:?}", tokens[*index]);
         return Err(String::from("invalid expression"));
     }
 
     }
 }
 
-// Rust will then run all the functions annotated with the "#[test]" keyword.
+
+// writing tests!
 #[cfg(test)]
 mod tests {
     use crate::lex;
-    use crate::parse_math;
+    use crate::parse_statement;
 
     #[test]
-    fn parser_test() {
-        // test that parser works on correct cases
-        assert!(parse_math_string("1;") == 1);
-        assert!(parse_math_string("1 + 2;") == 3);
-        assert!(parse_math_string("(7 * 6);") == 42);
-        assert!(parse_math_string("(7 * 6) + 42;") == 84);
-        assert!(parse_math_string("42 + (7 * 3) * 2;") == 84);
-        assert!(parse_math_string("2 + (7 * 3) + 2;") == 25);
-        assert!(parse_math_string("2 + (7 * 3) + 2;") == 25);
-        assert!(parse_math_string("2 + (7 * (3 + 1)) + 2;") == 32);
-        assert!(parse_math_string("(2);") == 2);
+    fn test_statements() {
 
-        // test parser errors
-        assert!(matches!(parse_error("(2;"), Err(_)));
-        assert!(matches!(parse_error("(2"), Err(_)));
-        assert!(matches!(parse_error("2);"), Err(_)));
-        assert!(matches!(parse_error("2))"), Err(_)));
-        assert!(matches!(parse_error("2 2;"), Err(_)));
-        assert!(matches!(parse_error("5 200;"), Err(_)));
-        assert!(matches!(parse_error("5 +- 200;"), Err(_)));
+        // test that valid statements are correct.
+        let tokens = lex("a = 1 + 2;").unwrap();
+        parse_statement(&tokens, &mut 0).unwrap();
+
+        let tokens = lex("b = 1 / 2;").unwrap();
+        parse_statement(&tokens, &mut 0).unwrap();
+
+
+        // test errors. missing semicolon
+        let tokens = lex("b = 1 / 2").unwrap();
+        assert!(matches!(parse_statement(&tokens, &mut 0), Err(_)));
+
     }
 
-    fn parse_error(expression: &str) -> Result<i32, String> {
-        let toks = lex(expression).unwrap();
-        parse_math(&toks, &mut 0)
-    }
-
-    fn parse_math_string(expression: &str) -> i32 {
-        let toks = lex(expression).unwrap();
-        parse_math(&toks, &mut 0).unwrap()
-    }
 }
-
-
 
